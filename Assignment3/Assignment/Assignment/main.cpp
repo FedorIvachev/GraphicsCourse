464027f75290a55d1184cceeb8592889e2058f17
FedorIvachev <feodor.ivachev@gmail.com>
2020-04-13 02:10:29 +0300

    Added moon, text help on the screen (TODO: planet names)

--- Assignment3/Assignment/Assignment/main.cpp
+++ Assignment3/Assignment/Assignment/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <map>
+#include <string>
 
 // GLAD
 #include <glad/glad.h>
@@ -11,6 +13,10 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+// FreeType
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 // Other includes
 #include "shader.h"
@@ -34,6 +40,7 @@ bool loadOBJ(const char* path, std::vector <glm::vec3 >& out_vertices,
     std::vector < glm::vec2 >& out_uvs,
     std::vector < glm::vec3 >& out_normals);
 GLuint loadTexture(const char* path, GLboolean alpha);
+void RenderText(Shader& shader, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color);
 
 
 class planet
@@ -43,12 +50,25 @@ public:
     GLfloat distance;
     GLuint texture;
     GLfloat scale;
+    GLfloat x;
+    GLfloat y;
 };
 
 planet planets[8];
 
 // Window dimensions
 GLuint WIDTH = 800, HEIGHT = 600;
+
+/// Holds all state information relevant to a character as loaded using FreeType
+struct Character {
+    GLuint TextureID;   // ID handle of the glyph texture
+    glm::ivec2 Size;    // Size of glyph
+    glm::ivec2 Bearing;  // Offset from baseline to left/top of glyph
+    GLuint Advance;    // Horizontal offset to advance to next glyph
+};
+
+std::map<GLchar, Character> Characters;
+GLuint text_VAO, text_VBO;
 
 // Camera
 Camera camera(glm::vec3(0.0f, 10.0f, 10.0f));
@@ -96,6 +116,97 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    // Set OpenGL options
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // Compile and setup the text shader
+    Shader text_shader("text.vert.glsl", "text.frag.glsl");
+    glm::mat4 text_projection = glm::ortho(0.0f, static_cast<GLfloat>(WIDTH), 0.0f, static_cast<GLfloat>(HEIGHT));
+    text_shader.Use();
+    glUniformMatrix4fv(glGetUniformLocation(text_shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(text_projection));
+
+
+    // FreeType
+    FT_Library ft;
+    // All functions return a value different than 0 whenever an error occurred
+    if (FT_Init_FreeType(&ft))
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+    
+    // Load font as face
+    FT_Face face;
+    if (FT_New_Face(ft, "C:/Windows/Fonts/Arial.ttf", 0, &face)) { // only absolute path works
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return 0;
+    }
+    
+    // Set size to load glyphs as
+    FT_Set_Pixel_Sizes(face, 0, 48);
+    
+    // Disable byte-alignment restriction
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    // Load first 128 characters of ASCII set
+    for (GLubyte c = 0; c < 128; c++)
+    {
+        // Load character glyph
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        // Generate texture
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+        // Set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // Now store character for later use
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            GLuint(face->glyph->advance.x)
+        };
+        Characters.insert(std::pair<GLchar, Character>(c, character));
+    }
+
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+    // Destroy FreeType once we're finished
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+
+
+    // Configure VAO/VBO for texture quads
+    glGenVertexArrays(1, &text_VAO);
+    glGenBuffers(1, &text_VBO);
+    glBindVertexArray(text_VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, text_VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+
 
     glEnable(GL_DEPTH_TEST);
 
@@ -187,6 +298,17 @@ int main()
         glClearColor(0, 0, 0, 0.5f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        RenderText(text_shader, "Use W, A, S, D and your mouse to move camera, use left/right arrow keys to change speed", 25.0f, 25.0f, 0.3f, glm::vec3(0.5, 0.5f, 0.8f));
+        std::string speed_text = "Current Speed : " + std::to_string(speed);
+        RenderText(text_shader, speed_text, 25.0f, 10.0f, 0.3f, glm::vec3(0.5, 0.5f, 0.8f));
+
+        //for (int i = 0; i < 8; i++) {
+            //RenderText(text_shader, "Sample text", , 0.3f, glm::vec3(0.5, 0.5f, 0.8f));
+        //}
+
+
+
+
         // Activate shader
         
         ourShader.Use();
@@ -217,6 +339,8 @@ int main()
             glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model_vertex));
 
             glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+            
+
         }
 
         // Render sun
@@ -226,6 +350,18 @@ int main()
         glm::mat4 model_vertex(1);
         model_vertex = glm::translate(model_vertex,
             glm::vec3(0.0f, 0.0f, 0.0f));
+
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model_vertex));
+
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+
+
+        // Render moon
+        glUniform1f(scaleLoc, 0.3f);
+        glBindVertexArray(VAO);
+        glBindTexture(GL_TEXTURE_2D, moonTexture);
+        model_vertex = glm::translate(model_vertex,
+            glm::vec3(planets[2].distance * sin(planets[2].angle) + planets[2].distance / 5 * cos(planets[2].angle * 28), 0.0f, planets[2].distance * cos(planets[2].angle) + planets[2].distance / 5 * sin(planets[2].angle * 28)));
 
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model_vertex));
 
@@ -288,9 +424,9 @@ void do_movement()
 void change_speed()
 {
     if (keys[GLFW_KEY_RIGHT])
-        speed += 0.000001;
+        speed += 0.0001;
     if (keys[GLFW_KEY_LEFT])
-        speed -= 0.000001;
+        speed -= 0.0001;
 }
 
 
@@ -418,4 +554,49 @@ GLuint loadTexture(const char* path, GLboolean alpha)
 
     SOIL_free_image_data(image);
     return textureID;
+}
+
+void RenderText(Shader& shader, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
+{
+    // Activate corresponding render state
+    shader.Use();
+    glUniform3f(glGetUniformLocation(shader.Program, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(text_VAO);
+
+    // Iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = Characters[*c];
+
+        GLfloat xpos = x + ch.Bearing.x * scale;
+        GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        GLfloat w = ch.Size.x * scale;
+        GLfloat h = ch.Size.y * scale;
+        // Update VBO for each character
+        GLfloat vertices[6][4] = {
+            { xpos,     ypos + h,   0.0, 1.0 },
+            { xpos,     ypos,       0.0, 0.0 },
+            { xpos + w, ypos,       1.0, 0.0 },
+
+            { xpos,     ypos + h,   0.0, 1.0 },
+            { xpos + w, ypos,       1.0, 0.0 },
+            { xpos + w, ypos + h,   1.0, 1.0 }
+        };
+        // Render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // Update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, text_VBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // Render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
--- Assignment3/Assignment/Assignment/main.frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 410 core
-
-out vec4 color;
-
-void main()
-{
-    color = vec4(0.5f, 1.0f, 0.5f, 1.0f);
-}
-
